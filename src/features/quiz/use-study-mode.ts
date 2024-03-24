fdeda7b841f6c9f2208f16af6ac2f510e75b74d1
nesso <32949286+nesso-pfl@users.noreply.github.com>
2024-03-24 20:18:07 +0900

    ok

--- src/features/quiz/use-study-mode.ts
+++ src/features/quiz/use-study-mode.ts
@@ -3,12 +3,17 @@ import { useLocalStorage, booleanCodec } from "../local-storage";
 const localStorageKey = "kinoden-pfl:studyMode";
 
 export const useStudyMode = () => {
-  const { value: studyMode = false, setValue: setStudyMode } = useLocalStorage<boolean>(localStorageKey, {
+  const {
+    value: studyMode = false,
+    setValue: setStudyMode,
+    isLoading,
+  } = useLocalStorage<boolean>(localStorageKey, {
     ...booleanCodec,
   });
 
   return {
     studyMode,
     setStudyMode,
+    isLoading,
   };
 };
