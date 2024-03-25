a85e3ebac6cb09e3f45870917664d3c272886d8b
nesso <32949286+nesso-pfl@users.noreply.github.com>
2024-03-26 05:01:16 +0900

    ok

---
+++ src/components/ui/header.tsx
@@ -0,0 +1,27 @@
+import { pagesPath } from "@/features/path/$path";
+import Link from "next/link";
+import React from "react";
+
+export const Header: React.FC = () => {
+  return (
+    <header className="flex justify-center bg-primary">
+      <div className="flex items-center w-full max-w-7xl text-white">
+        <div className="text-xl">Kinoden Pfl</div>
+        <nav className="ml-8">
+          <ul className="flex gap-4">
+            <li>
+              <Link className="block p-2" href={pagesPath.$url()}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link className="block p-2" href={pagesPath.quiz.$url()}>
+                Quiz
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
