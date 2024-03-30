9f831da7217dc3fe51d483437b8a0221b9067240
nesso <32949286+nesso-pfl@users.noreply.github.com>
2024-03-30 14:08:50 +0900

    ok

--- src/app/quiz/help-tooltip.tsx
+++ src/app/quiz/help-tooltip.tsx
@@ -1,15 +1,23 @@
 import { HelpCircleIcon } from "lucide-react";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import { useState } from "react";
 
 type HelpTooltipProps = {
   children: React.ReactNode;
 };
 
 export const HelpTooltip: React.FC<HelpTooltipProps> = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <TooltipProvider>
-      <Tooltip delayDuration={0}>
-        <TooltipTrigger className="w-min">
+      <Tooltip open={isOpen} delayDuration={0} onOpenChange={setIsOpen}>
+        <TooltipTrigger
+          className="w-min"
+          onClick={() => setIsOpen((prevOpen) => !prevOpen)}
+          onFocus={() => setTimeout(() => setIsOpen(true), 0)} // timeout needed to run this after onOpenChange to prevent bug on mobile
+          onBlur={() => setIsOpen(false)}
+        >
           <HelpCircleIcon />
         </TooltipTrigger>
         <TooltipContent>
