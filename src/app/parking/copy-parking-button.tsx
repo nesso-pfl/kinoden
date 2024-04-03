af3fd185a6fca7a90e28ebdcc349064d59c195ed
nesso <32949286+nesso-pfl@users.noreply.github.com>
2024-04-03 12:49:17 +0900

    ok

---
+++ src/app/parking/copy-parking-button.tsx
@@ -0,0 +1,138 @@
+"use client";
+
+import React, { useCallback, useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Parking, ParkingServer } from "@/features/parking";
+import dayjs from "dayjs";
+import { Input } from "@/components/ui/input";
+import { Checkbox } from "@/components/ui/checkbox";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Label } from "@/components/ui/label";
+import { z } from "zod";
+import { useForm, Controller } from "react-hook-form";
+import { Slider } from "@/components/ui/slider";
+import { FileDiff } from "lucide-react";
+
+type Props = {
+  parkings: Parking[];
+  parkingServers: ParkingServer[];
+};
+
+const showNumberMap: Record<number, string> = {
+  1: "①",
+  2: "②",
+  3: "③",
+  4: "④",
+  5: "⑤",
+  6: "⑥",
+  7: "⑦",
+  8: "⑧",
+  9: "⑨",
+  10: "⑩",
+  11: "⑪",
+  12: "⑫",
+};
+
+const formatParkings = (
+  parkings: Parking[],
+  parkingServers: ParkingServer[],
+  {
+    battleFilter,
+    openWithinHour,
+  }: {
+    battleFilter: "both" | "attack-only" | "defence-only";
+    openWithinHour: number[];
+  },
+) => {
+  return parkingServers
+    .filter((server) => (battleFilter === "both" ? true : battleFilter === "attack-only" ? !server.self : server.self))
+    .map((parkingServer) => {
+      const parkingTexts = parkings
+        .filter(
+          (parking) =>
+            parking.owner.id === parkingServer.id &&
+            dayjs().isBefore(dayjs(parking.open_at).add(openWithinHour, "hour")),
+        )
+        .map((parking) => `${showNumberMap[parking.number]}${dayjs(parking.open_at).format("HH:mm")}`)
+        .join(" ");
+
+      return parkingTexts ? `【${parkingServer.self ? "🛡️" : ""}${parkingServer.name}】${parkingTexts}` : "";
+    });
+};
+
+const battleFilters = ["both", "attack-only", "defence-only"] as const;
+type BattleFilter = (typeof battleFilters)[number];
+
+const radioOptions = [
+  { value: "both", label: "両方" },
+  { value: "attack-only", label: "奪取" },
+  { value: "defence-only", label: "防衛" },
+];
+
+const formSchema = z.object({
+  battleFilter: z.enum(battleFilters),
+  openWithinHour: z.number().array(),
+});
+
+type Form = z.infer<typeof formSchema>;
+
+export const CopyParkingButton: React.FC<Props> = ({ parkings, parkingServers }) => {
+  const { control, watch } = useForm<Form>({
+    defaultValues: {
+      battleFilter: "both",
+      openWithinHour: [4],
+    },
+  });
+  const battleFilter = watch("battleFilter");
+  const openWithinHour = watch("openWithinHour");
+
+  return (
+    <Dialog>
+      <Button asChild>
+        <DialogTrigger>スケジュールをコピー</DialogTrigger>
+      </Button>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>スケジュールをコピー</DialogTitle>
+        </DialogHeader>
+        <div className="text-lg text-start">フィルタ</div>
+        <Controller
+          control={control}
+          name="battleFilter"
+          render={({ field }) => (
+            <RadioGroup className="flex gap-4 mb-4" defaultValue="both" onValueChange={field.onChange}>
+              {radioOptions.map((option) => (
+                <div key={option.value} className="flex items-center space-x-2">
+                  <RadioGroupItem value={option.value} id={option.value} />
+                  <Label htmlFor={option.value}>{option.label}</Label>
+                </div>
+              ))}
+            </RadioGroup>
+          )}
+        />
+        <Controller
+          control={control}
+          name="openWithinHour"
+          render={({ field }) => (
+            <div>
+              <Slider min={1} max={4} step={1} value={field.value} onValueChange={field.onChange} /> {field.value}{" "}
+              時間以内に停戦終了のみ
+            </div>
+          )}
+        />
+
+        <pre className="border border-gray-400 rounded-md p-2 whitespace-normal break-all">
+          {formatParkings(parkings, parkingServers, { battleFilter, openWithinHour })}
+        </pre>
+      </DialogContent>
+    </Dialog>
+  );
+};
--- src/app/parking/parking-summary.tsx
+++ src/app/parking/parking-summary.tsx
@@ -3,6 +3,7 @@
 import { useParking } from "@/features/parking";
 import React from "react";
 import { ParkingSummaryItem } from "./parking-summary-item";
+import { CopyParkingButton } from "./copy-parking-button";
 
 type Props = {};
 
@@ -10,17 +11,20 @@ export const ParkingSummary: React.FC<Props> = () => {
   const { parkings, parkingServers } = useParking();
 
   return (
-    <div className="grid grid-cols-[32px_24px_5fr_5fr_5fr]">
-      <div className="grid items-center gap-2 col-span-5 grid-cols-subgrid p-2 border-b border-b-gray-400 last:border-0">
-        <div></div>
-        <div>No</div>
-        <div>所有サーバー</div>
-        <div>停戦終了日</div>
-        <div>停戦終了時刻</div>
+    <div>
+      <CopyParkingButton parkings={parkings} parkingServers={parkingServers} />
+      <div className="grid grid-cols-[32px_24px_5fr_5fr_5fr]">
+        <div className="grid items-center gap-2 col-span-5 grid-cols-subgrid p-2 border-b border-b-gray-400 last:border-0">
+          <div></div>
+          <div>No</div>
+          <div>所有サーバー</div>
+          <div>停戦終了日</div>
+          <div>停戦終了時刻</div>
+        </div>
+        {parkings.map((parking) => (
+          <ParkingSummaryItem key={parking.id} {...parking} parkingServers={parkingServers} />
+        ))}
       </div>
-      {parkings.map((parking) => (
-        <ParkingSummaryItem key={parking.id} {...parking} parkingServers={parkingServers} />
-      ))}
     </div>
   );
 };
