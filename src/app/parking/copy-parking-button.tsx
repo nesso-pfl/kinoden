fc668164a382f563bdc6e6630e1cc16482bde6f8
nesso <32949286+nesso-pfl@users.noreply.github.com>
2024-04-03 15:09:17 +0900

    ok

--- src/app/parking/copy-parking-button.tsx
+++ src/app/parking/copy-parking-button.tsx
@@ -1,25 +1,13 @@
-"use client";
-
-import React, { useCallback, useMemo, useState } from "react";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
+import React, { useMemo } from "react";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { Parking, ParkingServer } from "@/features/parking";
 import dayjs from "dayjs";
-import { Input } from "@/components/ui/input";
-import { Checkbox } from "@/components/ui/checkbox";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Label } from "@/components/ui/label";
 import { z } from "zod";
 import { useForm, Controller } from "react-hook-form";
 import { Slider } from "@/components/ui/slider";
-import { FileDiff } from "lucide-react";
 
 type Props = {
   parkings: Parking[];
@@ -124,7 +112,7 @@ export const CopyParkingButton: React.FC<Props> = ({ parkings, parkingServers })
           control={control}
           name="battleFilter"
           render={({ field }) => (
-            <RadioGroup className="flex gap-4 mb-4" defaultValue="both" onValueChange={field.onChange}>
+            <RadioGroup className="flex gap-4 mb-4" defaultValue="attack-only" onValueChange={field.onChange}>
               {radioOptions.map((option) => (
                 <div key={option.value} className="flex items-center space-x-2">
                   <RadioGroupItem value={option.value} id={option.value} />
@@ -139,8 +127,8 @@ export const CopyParkingButton: React.FC<Props> = ({ parkings, parkingServers })
           name="openWithinHour"
           render={({ field }) => (
             <div className="mb-8 select-none">
-              <Slider min={1} max={4} step={1} value={field.value} onValueChange={field.onChange} /> {openWithinHour}{" "}
-              時間以内に停戦終了のみ
+              <Slider className="mb-2" min={1} max={4} step={1} value={field.value} onValueChange={field.onChange} />
+              <div className="text-sm">{openWithinHour}&ensp;時間以内に停戦終了のみ</div>
             </div>
           )}
         />
