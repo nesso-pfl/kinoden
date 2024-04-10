bdeab02824119da6d0f4eb6a306ec27d4fe4bbe6
nesso <32949286+nesso-pfl@users.noreply.github.com>
2024-04-10 15:49:09 +0900

    ok

--- src/features/build/use-builds.ts
+++ src/features/build/use-builds.ts
@@ -1,4 +1,4 @@
-import { Build } from ".";
+import { Build, Skill } from ".";
 import { supabase } from "../supabase";
 import useSWR from "swr";
 
@@ -16,12 +16,19 @@ export const getBuilds = async (): Promise<
   return (
     response.data?.map((build) => ({
       ...build,
-      fellows: build.build_fellows.map((build_fellow) => build_fellow.fellows!),
-      labels: build.build_labels.map((build_label) => build_label.labels!).toSorted((l1, l2) => l1.order - l2.order),
-      skills: build.build_skills.map((build_skill) => ({
-        ...build_skill,
-        skill: build_skill.skills!,
-      })),
+      fellows: build.build_fellows
+        .map((build_fellow) => build_fellow.fellows)
+        .filter((fellow): fellow is Exclude<typeof fellow, null> => !!fellow),
+      labels: build.build_labels
+        .map((build_label) => build_label.labels)
+        .filter((label): label is Exclude<typeof label, null> => !!label)
+        .toSorted((l1, l2) => l1.order - l2.order),
+      skills: build.build_skills
+        .map((build_skill) => ({
+          ...build_skill,
+          skill: build_skill.skills,
+        }))
+        .filter((skill): skill is typeof skill & { skill: Exclude<typeof skill.skill, null> } => !!skill.skill),
     })) ?? []
   );
 };
