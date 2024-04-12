d6eb82776e26deb4dd6d8828b9523f4e45cf25bf
nesso <32949286+nesso-pfl@users.noreply.github.com>
2024-04-12 16:19:57 +0900

    ok

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,20 @@
 import bundleAnalyzer from '@next/bundle-analyzer';
+import nextPwa from 'next-pwa';
+
+const withPwa = nextPwa({
+  dest: 'public'
+})
 
 const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
 })
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withPwa({
   images: {
     unoptimized: true
   },
   output: 'export'
-};
+});
 
 export default withBundleAnalyzer(nextConfig);
