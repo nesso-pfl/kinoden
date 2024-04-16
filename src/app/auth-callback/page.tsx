7689de4d571b4f1b9fccdbc0df5b6fd4ecae2645
nesso <32949286+nesso-pfl@users.noreply.github.com>
2024-04-16 11:23:19 +0900

    ok

---
+++ src/app/auth-callback/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Metadata } from "next";
+import { SetAuthSession } from "@/features/auth";
+
+export const metadata: Metadata = {
+  title: "ログイン中 | Kinoden Pfl",
+};
+
+export default function Page() {
+  return (
+    <div>
+      <SetAuthSession />
+    </div>
+  );
+}
