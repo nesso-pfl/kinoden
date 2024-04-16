01dfd4506006a26333818cbb3b5bb3ca7fb33a8d
nesso <32949286+nesso-pfl@users.noreply.github.com>
2024-04-16 13:58:29 +0900

    ok

--- src/features/auth/set-auth-session.tsx
+++ src/features/auth/set-auth-session.tsx
@@ -2,26 +2,40 @@
 
 import React, { useEffect, useRef, useState } from "react";
 import { supabase } from "../supabase";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
+import { pagesPath } from "../path/$path";
+
+const getCurrentSession = (): { access_token: string; refresh_token: string } => {
+  const hash = typeof window !== "undefined" ? decodeURIComponent(window.location.hash.replace("#", "")) : undefined;
+  const sessionObj = Object.fromEntries(hash?.split("&").map((s) => s.split("=")) ?? []);
+
+  if ("access_token" in sessionObj && "refresh_token" in sessionObj) {
+    return sessionObj;
+  } else {
+    throw new Error("No session found");
+  }
+};
 
 const useSetAuthSession = () => {
   const searchParams = useSearchParams();
+  const router = useRouter();
   const [loading, setLoading] = useState(true);
   const emitted = useRef(false);
 
   useEffect(() => {
-    const access_token = searchParams.get("access_token");
-    const refresh_token = searchParams.get("refresh_token");
+    const { access_token, refresh_token } = getCurrentSession();
+
     if (emitted.current || !access_token || !refresh_token) return;
 
     emitted.current = true;
     const fn = async () => {
       await supabase.auth.setSession({ access_token, refresh_token });
       setLoading(false);
+      router.replace(pagesPath.parking.$url().pathname);
     };
 
     fn();
-  }, [searchParams]);
+  }, [searchParams, router]);
 
   return { loading };
 };
