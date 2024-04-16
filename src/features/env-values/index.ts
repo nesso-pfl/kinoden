b11a2bbb3a5f3ba2d60139fd9c83bf6d5a3e9f5d
nesso <32949286+nesso-pfl@users.noreply.github.com>
2024-04-16 11:22:54 +0900

    ok

--- src/features/env-values/index.ts
+++ src/features/env-values/index.ts
@@ -1,4 +1,5 @@
 export const envValues = {
   supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!,
   anonKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  authCallbackUrl: process.env.NEXT_PUBLIC_AUTH_CALLBACK_URL!,
 } as const;
