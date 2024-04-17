9b5d5b6631b8d8834e28ec65a90240699a67a96f
nesso <32949286+nesso-pfl@users.noreply.github.com>
2024-04-17 14:19:51 +0900

    ok

--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -1,4 +1,4 @@
 export * from "./set-auth-session";
 export * from "./sign-in-with-discord";
 export * from "./sign-in-with-google";
-export * from "./use-get-user";
+export * from "./use-user";
---
+++ src/features/auth/use-user.tsx
@@ -0,0 +1,23 @@
+import { supabase } from "../supabase";
+import { useEffect, useState } from "react";
+import { User } from "@supabase/supabase-js";
+
+type UserInfo = {
+  inited: boolean;
+  user: User | undefined;
+};
+
+export const useUser = () => {
+  const [userInfo, setUserInfo] = useState<UserInfo>({ inited: true, user: undefined });
+  useEffect(() => {
+    const { data } = supabase.auth.onAuthStateChange((_, session) => {
+      setUserInfo({ inited: true, user: session?.user ?? undefined });
+    });
+
+    return () => {
+      data.subscription.unsubscribe();
+    };
+  });
+
+  return userInfo;
+};
