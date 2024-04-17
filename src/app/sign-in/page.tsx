6cebd8f8878282313b1467817e30e1b766e12c90
nesso <32949286+nesso-pfl@users.noreply.github.com>
2024-04-17 12:47:03 +0900

    ok

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -10,7 +10,12 @@ export default function Page() {
   return (
     <div>
       <h1 className="text-2xl mb-8">ログイン</h1>
-      <div className="flex flex-col items-center w-full mb-8">
+      <div className="flex flex-col items-center w-full">
+        <p className="mb-8">
+          以下の三種類のいずれかをご利用ください。
+          <br />
+          Discord か、捨てアドでの Google ログインがおすすめです。
+        </p>
         <SignInButtons />
       </div>
     </div>
--- src/app/sign-in/sign-in-buttons.tsx
+++ src/app/sign-in/sign-in-buttons.tsx
@@ -4,7 +4,6 @@ import React from "react";
 import { Auth } from "@supabase/auth-ui-react";
 import { supabase } from "@/features/supabase";
 import { envValues } from "@/features/env-values";
-import { ThemeSupa } from "@supabase/auth-ui-shared";
 
 export const SignInButtons = () => {
   return (
@@ -13,7 +12,12 @@ export const SignInButtons = () => {
       providers={["discord", "twitter", "google"]}
       redirectTo={envValues.authCallbackUrl}
       appearance={{
-        theme: ThemeSupa,
+        extend: false,
+        className: {
+          container: "flex flex-col gap-2",
+          button:
+            "flex gap-2 items-center w-52 py-2 pl-4 rounded-md border bg-white text-gray-700 hover:bg-gray-500/10",
+        },
       }}
       onlyThirdPartyProviders
     />
