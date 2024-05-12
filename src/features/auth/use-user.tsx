643dc85e4b31f2a5f768d165a34ae5db03061ac9
nesso <32949286+nesso-pfl@users.noreply.github.com>
2024-05-13 04:14:44 +0900

    ok

--- src/features/auth/use-user.tsx
+++ src/features/auth/use-user.tsx
@@ -6,7 +6,6 @@ import { create } from "zustand";
 import { User } from "./user";
 
 const getUserProfile = async (userId: string) => {
-  console.log("getUserProfile");
   const response = await supabase
     .from("user_profiles")
     .select(
@@ -18,7 +17,6 @@ const getUserProfile = async (userId: string) => {
     .eq("user_id", userId)
     .single();
 
-  console.log(response);
   return response;
 };
 
@@ -39,8 +37,6 @@ export const useUserStore = create<UserStore>()((set) => ({
 export const useUser = () => {
   const { inited, authenticated, user } = useUserStore();
 
-  console.log(user, inited, authenticated);
-
   return { user, inited, signedIn: authenticated };
 };
 
@@ -48,24 +44,15 @@ export const useUserUpdate = () => {
   const { updateUser } = useUserStore();
 
   useEffect(() => {
-    console.log("subscribe");
     const { data } = supabase.auth.onAuthStateChange(async (_, session) => {
-      console.log(location);
       if (!session) {
-        console.log({ user: undefined, authenticated: false, inited: true });
         updateUser({ user: undefined, authenticated: false, inited: true });
         return;
       }
       const response = await getUserProfile(session.user.id);
       if (response.error) {
-        console.log({ user: undefined, authenticated: true, inited: true });
         updateUser({ user: undefined, authenticated: true, inited: true });
       } else {
-        console.log({
-          user: { ...response.data, role: response.data.user_roles?.role },
-          authenticated: true,
-          inited: true,
-        });
         updateUser({
           user: { ...response.data, role: response.data.user_roles?.role },
           authenticated: true,
@@ -75,7 +62,6 @@ export const useUserUpdate = () => {
     });
 
     return () => {
-      console.log("unsubscribe");
       data.subscription.unsubscribe();
     };
   }, [updateUser]);
